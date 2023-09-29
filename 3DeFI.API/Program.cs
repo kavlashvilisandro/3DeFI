8528aafeb6baa836679946767d85eb76cb75d03a
kavlashvilisandro <skavl21@freeuni.edu.ge>
2023-09-29 17:34:00 +0400

    token auth parameters added

--- 3DeFI.API/Program.cs
+++ 3DeFI.API/Program.cs
@@ -1,10 +1,30 @@
+using System.Text;
+using _3DeFI.API.Application;
 using _3DeFI.API.Presentation;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+//Services
+builder.Services.AddSingleton<IJWTService, JWTService>();
 
+//Repos
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication()
+    .AddJwtBearer("UserAuth", (options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = false,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey =
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("JWT:PrivateKey"))),
+            ValidateAudience = false
+        };
+    }));
 
 var app = builder.Build();
 
